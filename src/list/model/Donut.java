61734a1fd0d77a5969aba20ec4f22de0f0239849
greenRose2018 <brifun13@gmail.com>
2017-10-03 08:44:32 -0600

    Added datamembers

--- src/list/model/Donut.java
+++ src/list/model/Donut.java
@@ -2,6 +2,13 @@ package list.model;
 
 public class Donut
 {
+	private boolean hasSprinkes;
+	private boolean isGlazed;
+	private String shape;
+	private String flavor;
+	private int holeCount;
+	
+	
 	public Donut()
 	{
 		
