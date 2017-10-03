c9e617d8c168118d689402c183c94a4e1874bc8c
greenRose2018 <brifun13@gmail.com>
2017-10-03 08:59:47 -0600

    Notes/steps on how to make an arrayList

--- src/list/controller/ListController.java
+++ src/list/controller/ListController.java
@@ -1,19 +1,22 @@
 package list.controller;
 
-import java.util.List;
+import java.util.List;			//step 1: import donut file, list and arraylist.
 import java.util.ArrayList;
 import list.model.Donut;
 
 public class ListController
 {
-	private List<Donut> donutList;
+	private List<Donut> donutList; //step 2: declare 
 	
 	public ListController()
 	{
-		
+		donutList = new ArrayList<Donut>(); //step 3: initialize need to call using params
 	}
 	public void start()
 	{
+		//temporarily make Donut an object
+		Donut temp = new Donut();
 		
+		donutList.add(temp); // to here
 	}
 }
