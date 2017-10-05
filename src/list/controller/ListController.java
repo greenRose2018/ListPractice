97251180b6a4f0a0b7b32c604fe6b4491595f992
greenRose2018 <brifun13@gmail.com>
2017-10-05 09:50:51 -0600

    practice loops and used if and for statements

--- src/list/controller/ListController.java
+++ src/list/controller/ListController.java
@@ -18,17 +18,35 @@ public class ListController
 	public void start()
 	{
 		//temporarily makes Donut an object
-		Donut temp = new Donut();
+		//Donut temp = new Donut();
 		
-		donutList.add(temp); // to here
+		//donutList.add(temp); // to here
 		fillTheList();
 		showTheList();
 	}
 	
 	private void showTheList()
 	{
+		String favorite = "ChocolateFilled";
 		for (int index = 0; index < donutList.size(); index++)
 		{
+			String currentFlavor = donutList.get(index).getFlavor();
+			
+			Donut currentDonut = donutList.get(index);
+			String flavor = currentDonut.getFlavor();
+			
+			if (currentFlavor.equals(favorite))
+			{
+				for (int woop = 1; woop < 2; woop += 1)
+				{
+					display.displayText("The best Flavor ever!!");
+				}	
+			}
+			/*for (int currentLetterIndex = 0; currentLetterIndex < currentFlavor.length(); currentLetterIndex  += 1)
+			{
+				display.displayText(currentFlavor.substring(currentLetterIndex,  currentLetterIndex+ 1));
+			}*/
+			
 			display.displayText(donutList.get(index).toString());
 		}
 	}
